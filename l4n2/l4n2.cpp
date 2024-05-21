779e2671f2dd9501616945ce742331b04a43372e
Sanligod <Sanligod@yandex.ru>
2024-05-21 11:08:54 +0300

    Добавьте файлы проекта.

---
+++ l4n2/l4n2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+double F(double u, double t) {
+    if (u > 0) {
+        return u + sin(t);
+    }
+    else {
+        return u + t;
+    }
+}
+
+int main() {
+    setlocale(0, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, b;
+    std::cout << "Введите действительные a и b ";
+    std::cin >> a >> b;
+    if (std::cin.fail()) {
+        std::cout << "Введите цифры ";
+        return 1;
+    }
+
+    double z = F(sin(a), b) + F(cos(a), b) + F(pow(sin(a), 2), a - 1) + F(pow(sin(a), 2), cos(b));
+
+    std::cout << "z= " << z << std::endl;
+
+    return 0;
+}
